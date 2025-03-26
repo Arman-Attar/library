40fb6c3b6359fd845c4a39359bec41612df64550
Arman <arman.zattar@gmail.com>
2025-03-25 23:18:53 -0400

    finished styling the cards to display books

--- script.js
+++ script.js
@@ -21,6 +21,44 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayEachBook() {
     myLibrary.forEach(book => {
-        console.log(book.info())
+        const bookItem = document.createElement('div')
+        const buttons = document.createElement('div')
+        buttons.setAttribute('class', 'buttons')
+        bookItem.setAttribute('class', "book-card")
+        const bookTitle = document.createElement('h3')
+        const bookAuthor = document.createElement('h5')
+        const bookPages = document.createElement('h5')
+        bookTitle.innerText = book.title
+        bookAuthor.innerText = book.author
+        bookPages.innerText = book.pages + " Pages"
+        bookItem.appendChild(bookTitle)
+        bookItem.appendChild(bookAuthor)
+        bookItem.appendChild(bookPages)
+        const readButton = document.createElement('button')
+        readButton.setAttribute('class', "read-status")
+        book.read = true ? readButton.innerText = "Read" : readButton.innerText = "Not Read"
+        const removeButton = document.createElement('button')
+        removeButton.setAttribute('class', "remove-book")
+        removeButton.innerText = "Remove from Library"
+        buttons.appendChild(readButton)
+        buttons.appendChild(removeButton)
+        bookItem.appendChild(buttons) 
+        bookshelf.appendChild(bookItem)
     });
 }
+
+
+
+const 
+
+const bookshelf = document.querySelector('.bookshelf')
+
+
+function startUp() {
+    addBookToLibrary("Crime and Punishment", "Fyodor Dostoevsky", 488, true)
+    addBookToLibrary("The Divine Comedy", "Dante Alighieri", 442, false)
+    addBookToLibrary("1984", "George Orwell", 328, true)
+    displayEachBook()
+}
+
+startUp()
